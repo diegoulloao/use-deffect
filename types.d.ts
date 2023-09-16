2c1ce87595fa41360c921c22d61d171d61dd4bbb
Diego <diegoulloao@icloud.com>
2023-09-16 19:27:18 -0300

    fix: return type and parameters

--- types.d.ts
+++ types.d.ts
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
 
 declare module "deffect" {
-  export function useDeffect(): typeof useEffect;
+  export function useDeffect(
+    ...args: Parameters<typeof useEffect>
+  ): ReturnType<typeof useEffect>;
 }
